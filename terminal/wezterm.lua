bb06e4658bb2f7a276b5b3b746ae82fa3dc26fe0
blackprince001 <appiahboaduprince@gmail.com>
2024-01-30 21:18:48 +0000

    update all config on local

--- terminal/wezterm.lua
+++ terminal/wezterm.lua
@@ -14,15 +14,19 @@ end
 
 -- For example, changing the color scheme:
 
-  config.font = wezterm.font 'Hack Nerd Font'
+  config.font = wezterm.font('Hack Nerd Font', { weight = 'Bold', italic = false })
   config.font_size = 10
-  config.initial_rows = 60
-  config.initial_cols = 140
-  config.line_height = 1
-  config.color_scheme = 'Monokai Remastered'
-  
+  config.initial_rows = 52
+  config.initial_cols = 130
+  config.line_height = 1.1
+  config.color_scheme = 'Hardcore'
+
   config.default_cursor_style = 'SteadyBar'
-  config.window_decorations = "NONE"
+  config.dpi = 96.0
+  config.prefer_egl = true
+ 
+  config.window_decorations = "None"
+  config.enable_scroll_bar = true
   
   config.command_palette_font_size = 10
   config.command_palette_fg_color = "rgba(206, 205, 195, 1.0)"
